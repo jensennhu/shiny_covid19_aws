ad8ceb32e77b8ab6ada9128d83893499120dd208
Jensen Hu <jensennhu@gmail.com>
2021-01-07 06:03:34 -0800

    update

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ source("function.R")
 ui <- fluidPage(theme = shinytheme("superhero"),
 
     # Application title
-    titlePanel("Low Budget COVID-19 Dash"),
+    titlePanel("The 'Rona Digest [beta]"),
     h4(paste0("Updated as of ", Sys.Date())),
 
     # Sidebar 
