2598d3f487731d0041853b1815c4bd91b57cf058
Jensen Hu <jensennhu@gmail.com>
2021-01-07 10:05:56 -0800

    updating shiny server

---
+++ update_aws.bat
@@ -0,0 +1,7 @@
+git add .
+git commit -m "updating shiny server"
+git push origin master
+cd .ssh/
+ssh -i "shiny-key.pem" ubuntu@ec2-3-17-139-199.us-east-2.compute.amazonaws.com
+cd /srv/shiny-server/shiny_covid19_aws/
+git pull origin master
